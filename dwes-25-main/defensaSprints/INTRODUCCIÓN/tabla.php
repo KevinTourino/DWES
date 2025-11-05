b89760215868bf0791bbc3cfe58eaac34b969ee4
KevinTT <ktourinot23@fpcoruna.afundacion.org>
2025-11-05 14:15:07 +0100

    Ejercicio tabla de multiplicar

--- dwes-25-main/defensaSprints/INTRODUCCIÓN/tabla.php
+++ dwes-25-main/defensaSprints/INTRODUCCIÓN/tabla.php
@@ -5,12 +5,14 @@
       <tr><th>Expresión</th><th>Resultado</th></tr>
       <?php
       /*Cambia el for por un while*/
-        for ($i = 1; $i <= 10; $i = $i + 1) {
+      $i = 0;
+        while($i<10) {
           $res = 7 * $i;
           echo "<tr>";
           echo "<td>7 x " . $i . "</td>";
           echo "<td>" . $res . "</td>";
           echo "</tr>";
+          $i=$i+1;
         }
       ?>
     </table>
