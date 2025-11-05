fab4ec18130ee14c0d5e03065530b24c94a59f3d
KevinTT <ktourinot23@fpcoruna.afundacion.org>
2025-11-05 14:12:53 +0100

    Ejercicio imc

--- dwes-25-main/defensaSprints/INTRODUCCIÓN/imc.php
+++ dwes-25-main/defensaSprints/INTRODUCCIÓN/imc.php
@@ -8,10 +8,13 @@
         return $peso / ($altura * $altura);
       }
 
-      if (isset($_GET["peso"]) && isset($_GET["altura"])) {
+      if (isset($_GET["edad"]) && isset($_GET["peso"]) && isset($_GET["altura"])) {
         $peso = $_GET["peso"];
         $altura = $_GET["altura"];
+        $edad = $_GET["edad"];
         $imc = calcular_imc($peso, $altura);
+
+        echo "<h3>Edad: " . $edad . "</h3>";
 
         if ($imc < 18.5) {
           echo "IMC: " . $imc . " → Bajo peso";
