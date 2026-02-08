ac203d804e995183f34b36817b27b8e89507f6f7
kevin <ktourinot23@fpcoruna.afundacion.org>
2026-02-08 23:19:34 +0100

    Semana2 bloque1

--- proyecto/cine/views.py
+++ proyecto/cine/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+﻿from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Pelicula
 
-# Create your views here.
+
+class PeliculaListAPIView(APIView):
+    def get(self, request):
+        peliculas = Pelicula.objects.all()
+        data = []
+        for pelicula in peliculas:
+            data.append({
+                'id': pelicula.id,
+                'titulo': pelicula.titulo,
+                'duracion': pelicula.duracion,
+                'fecha_estreno': pelicula.fecha_estreno,
+                'precio': str(pelicula.precio),
+                'disponible': pelicula.disponible
+            })
+        return Response(data)
+
+    def post(self, request):
+        try:
+            pelicula = Pelicula.objects.create(
+                titulo=request.data.get('titulo'),
+                duracion=request.data.get('duracion'),
+                fecha_estreno=request.data.get('fecha_estreno'),
+                precio=request.data.get('precio'),
+                disponible=request.data.get('disponible', True)
+            )
+            data = {
+                'id': pelicula.id,
+                'titulo': pelicula.titulo,
+                'duracion': pelicula.duracion,
+                'fecha_estreno': pelicula.fecha_estreno,
+                'precio': str(pelicula.precio),
+                'disponible': pelicula.disponible
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response(
+                {'error': str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class PeliculaDetailAPIView(APIView):
+    def get(self, request, pk):
+        try:
+            pelicula = Pelicula.objects.get(pk=pk)
+            data = {
+                'id': pelicula.id,
+                'titulo': pelicula.titulo,
+                'duracion': pelicula.duracion,
+                'fecha_estreno': pelicula.fecha_estreno,
+                'precio': str(pelicula.precio),
+                'disponible': pelicula.disponible
+            }
+            return Response(data)
+        except Pelicula.DoesNotExist:
+            return Response(
+                {'error': 'Pelicula no encontrada'},
+                status=status.HTTP_404_NOT_FOUND
+            )
--- proyecto/config/settings.py
+++ proyecto/config/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cine',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
