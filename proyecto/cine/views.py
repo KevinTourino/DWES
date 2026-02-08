8a3584d417dacbafe25faefb13aac4f17ba80881
kevin <ktourinot23@fpcoruna.afundacion.org>
2026-02-08 23:31:51 +0100

    Semana2 bloque3

--- proyecto/cine/views.py
+++ proyecto/cine/views.py
@@ -1,19 +1,18 @@
 ﻿from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .models import Pelicula
-from .serializers import PeliculaSerializer
+from rest_framework import status, viewsets
+from .models import Pelicula, Usuario
+from .serializers import PeliculaSerializer, UsuarioSerializer
 
 
+# ===== APIVIEW (Bloque 2) =====
 class PeliculaListAPIView(APIView):
     def get(self, request):
-        """GET lista - devuelve todas las peliculas"""
         peliculas = Pelicula.objects.all()
         serializer = PeliculaSerializer(peliculas, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        """POST - crea una nueva pelicula"""
         serializer = PeliculaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -23,7 +22,6 @@ class PeliculaListAPIView(APIView):
 
 class PeliculaDetailAPIView(APIView):
     def get(self, request, pk):
-        """GET detalle - devuelve una pelicula por id"""
         try:
             pelicula = Pelicula.objects.get(pk=pk)
             serializer = PeliculaSerializer(pelicula)
@@ -33,3 +31,14 @@ class PeliculaDetailAPIView(APIView):
                 {'error': 'Pelicula no encontrada'},
                 status=status.HTTP_404_NOT_FOUND
             )
+
+
+# ===== VIEWSETS (Bloque 3) =====
+class PeliculaViewSet(viewsets.ModelViewSet):
+    queryset = Pelicula.objects.all()
+    serializer_class = PeliculaSerializer
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
--- proyecto/config/urls.py
+++ proyecto/config/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path
-from cine.views import PeliculaListAPIView, PeliculaDetailAPIView
+﻿from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/peliculas/', PeliculaListAPIView.as_view(), name='pelicula-list'),
-    path('api/peliculas/<int:pk>/', PeliculaDetailAPIView.as_view(), name='pelicula-detail'),
+    path('', include('cine.urls')),
 ]
